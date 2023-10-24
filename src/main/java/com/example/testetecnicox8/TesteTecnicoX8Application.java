671377a6f1fce946e1fe91b4582c484e036af299
Gabriel Saturi <gabrielsaturi11@gmail.com>
2023-10-24 12:14:05 -0300

    Initial commit

---
+++ src/main/java/com/example/testetecnicox8/TesteTecnicoX8Application.java
@@ -0,0 +1,14 @@
+package com.example.testetecnicox8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteTecnicoX8Application {
+
+	public static void main(String[] args) {
+		System.out.println("salve ze");
+		SpringApplication.run(TesteTecnicoX8Application.class, args);
+	}
+
+}
